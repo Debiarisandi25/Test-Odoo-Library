3d6ce182ca1da6dc7208d4e1328473307a5ebf63
Debiarisandi25 <91276377+Debiarisandi25@users.noreply.github.com>
2026-01-02 16:25:18 +0700

    Create Manifest.py

---
+++ Manifest.py
@@ -0,0 +1,23 @@
+{
+    'name' : "Library Management",
+    'summary' : """
+        Manage your library books effectively.""", 
+    'description' : """
+        This module is usde to manage books like in a library, where user can read and borrow books. 
+    """,
+    'author' : "Debi Arisandi",
+    'category' : 'Inventory',
+    'version' : '0.1',
+    'data' : [
+        # 'views/views.xml',
+        # 'views/template.xml',
+        # 'security/ir.model.access.csv'
+        'views/library_views.xml'
+    
+    ],
+    'demo':[
+        'demo/demo.xml',
+    ],
+    'icon' : 'C:\Program Files\Odoo 19.0.20251231\server\odoo\addons\library\static\icon.png'
+
+}
